37a182073405fecadbb006ebec4ac59d2ecd2fb5
Roy <vargas.sap321@gmail.com>
2023-08-02 23:05:47 -0700

    Update base_model and create file __init__.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,12 +18,12 @@ class BaseModel():
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Returns the string representation of BaseModel."""
         return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, str(self.__dict__))
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at"""
@@ -32,7 +32,7 @@ class BaseModel():
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
         dict = self.__dict__.copy()
-        dict["__class__"] = self.__class__.__name__
         dict["created_at"] = self.created_at.isoformat()
         dict["updated_at"] = self.updated_at.isoformat()
+        dict["__class__"] = self.__class__.__name__
         return dict
